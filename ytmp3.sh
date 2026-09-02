02a0d37d3ffd15b154f86ce032e0239b436816bd
finnhp01 <finnhp01@gmail.com>
2026-09-02 22:43:36 +0200

    ...
    Updated the command, unexpected errors related to cookies fixed

--- ytmp3.sh
+++ ytmp3.sh
@@ -1,1 +1,12 @@
-yt-dlp -x --audio-format mp3 --embed-thumbnail --add-metadata -o "%(title)s.%(ext)s" "$@" 
+#!/bin/sh
+
+yt-dlp \
+    --cookies-from-browser firefox \
+    --js-runtimes node \
+    --remote-components ejs:github \
+    -x \
+    --audio-format mp3 \
+    --embed-thumbnail \
+    --add-metadata \
+    -o "%(title)s.%(ext)s" \
+    "$@"
